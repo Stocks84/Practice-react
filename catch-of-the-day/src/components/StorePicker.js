be15546237271165714f05591af4082d6253f5a0
William Stocks <willstocks@me.com>
2024-05-17 13:50:12 +0000

    Add goToStore function to StorePicker.js

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,24 @@ import React, { Fragment } from "react";
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        console.log(this)
+        // 3. change the page to /store/whatever-they-entered
+    }
     render() {
         return (
-             <form className="store-selector">
+             <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Pleae Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input 
+                type="text" 
+                ref={this.myInput} 
+                required placeholder="Store Name" 
+                defaultValue={getFunName()} />
                 <button type="submit">Visit Store</button>
             </form>
            
