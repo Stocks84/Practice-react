35225783e63b4a567d60807c4c428809a85098e9
William Stocks <willstocks@me.com>
2024-05-18 14:15:15 +0000

    Add function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,9 +8,10 @@ class StorePicker extends React.Component {
         // 1. Stop the form from submitting
         event.preventDefault();
         // 2. get the text from that input
-        console.log(this)
+        const storeName = this.myInput.current.value;
         // 3. change the page to /store/whatever-they-entered
-    }
+        this.props.history.push(`/store/${storeName}`);
+    };
     render() {
         return (
              <form className="store-selector" onSubmit={this.goToStore}>
