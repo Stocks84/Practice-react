ccb918ba4b0a51cdd6ee3e94c101b1752bdfe9a2
William Stocks <willstocks@me.com>
2024-05-20 12:53:45 +0000

    Add Fish.js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
@@ -16,15 +17,23 @@ class App extends React.Component {
         // 3. Set the new fishes object to state
         this.setState({ fishes });
     };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
-                
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />          
             </div>
         )
     }
